7f9acf1e8e48c8fbf87e3e5cb9def77bbcfeed98
codexcelsior <quang13012005@gmail.com>
2023-05-16 23:47:01 +0700

    Servo Complete

--- Training02/Servo.ino
+++ Training02/Servo.ino
@@ -18,6 +18,17 @@ Adafruit_PWMServoDriver pwm; //Hoặc Adafruit_PWMServoDriver pwm = new Adafruit
 #define PWM_SERVO3          11
 #define PWM_SERVO4          12
 
+// Độ rộng xung cho servo 360 (đã calibrate)
+#define SRV_360_T1          698 // độ rộng xung mà tại đó servo quay nhanh nhất theo 1 chiều
+#define SRV_360_T2          1362 // biên dưới của khoảng servo dừng quay
+#define SRV_360_T3          1440 // biên trên của khoảng servo dừng quay
+#define SRV_360_T4          2104 // độ rộng xung mà tại đó servo quay nhanh nhất theo chiều còn lại
+
+// Cổng của servo
+#define SRV_INTAKE          0
+
+// Tốc độ động cơ
+#define SPD_INTAKE          100 // servo
 
 
 void setup() { //Hàm set up chạy khởi tạo một lần khi khởi động mạch 
@@ -28,33 +39,12 @@ void setup() { //Hàm set up chạy khởi tạo một lần khi khởi động 
 
 }
 
-// Hàm điều khiển động cơ DC (nhận số động cơ từ 0->3 ứng với cặp kênh PWM 0-1/2-3/4-5/6-7, giá trị tốc độ từ -4095 đến 4095)
-void ctrl_dc(uint8_t motor, int16_t speed) {
-  switch(motor) {
-    case 0:
-      pwm.setPWM(PWM_DC0A, 0, (speed > 0) ? speed : 0);
-      pwm.setPWM(PWM_DC0B, 0, (speed < 0) ? (-speed) : 0);
-      break;
-    case 1:
-      pwm.setPWM(PWM_DC1A, 0, (speed > 0) ? speed : 0);
-      pwm.setPWM(PWM_DC1B, 0, (speed < 0) ? (-speed) : 0);
-      break;
-    case 2:
-      pwm.setPWM(PWM_DC2A, 0, (speed > 0) ? speed : 0);
-      pwm.setPWM(PWM_DC2B, 0, (speed < 0) ? (-speed) : 0);
-      break;
-    case 3:
-      pwm.setPWM(PWM_DC3A, 0, (speed > 0) ? speed : 0);
-      pwm.setPWM(PWM_DC3B, 0, (speed < 0) ? (-speed) : 0);
-      break;
-  }
-}
 
 // Hàm điều khiển vị trí servo 180 (nhận số động cơ servo từ 0->4 ứng với kênh PWM 8->12, giá trị góc từ 0 đến 180)
 void ctrl_servo180(uint8_t motor, float angle) {
   uint16_t us = (uint16_t) (1000 * (1.0 + angle / 180.0));
   switch(motor) {
-    case 0: pwm.writeMicroseconds(PWM_SERVO0, us); break;
+    case 0: pwm.writeMicroseconds(PWM_SERVO0, us); break; //writeMicrosecond dùng để cho Servo biét cần phải quay bao nhiều ms để quay đc 1 góc cần và đủ
     case 1: pwm.writeMicroseconds(PWM_SERVO1, us); break;
     case 2: pwm.writeMicroseconds(PWM_SERVO2, us); break;
     case 3: pwm.writeMicroseconds(PWM_SERVO3, us); break;
@@ -65,8 +55,10 @@ void ctrl_servo180(uint8_t motor, float angle) {
 // Hàm điều khiển tốc độ servo 360 (nhận số động cơ servo từ 0->4 ứng với kênh PWM 8->12, giá trị tốc độ từ -100 đến 100)
 void ctrl_servo360(uint8_t motor, float speed) {
   uint16_t us = (SRV_360_T2 + SRV_360_T3) / 2;
-  if(speed > 0) us = SRV_360_T3 + (speed / 100) * (SRV_360_T4 - SRV_360_T3);
-  else if(speed < 0) us = SRV_360_T2 + (speed / 100) * (SRV_360_T2 - SRV_360_T1);
+  if(speed > 0) 
+    us = SRV_360_T3 + (speed / 100) * (SRV_360_T4 - SRV_360_T3);
+  else if(speed < 0) 
+    us = SRV_360_T2 + (speed / 100) * (SRV_360_T2 - SRV_360_T1);
   switch(motor) {
     case 0: pwm.writeMicroseconds(PWM_SERVO0, us); break;
     case 1: pwm.writeMicroseconds(PWM_SERVO1, us); break;
@@ -79,6 +71,11 @@ void ctrl_servo360(uint8_t motor, float speed) {
 
 void loop() {
     // put your main code here, to run repeatedly: 
+    
+    bool intake = True;
+
+    //Hàm mẫu để sử dụng Servo 
+    ctrl_servo180(SRV_INTAKE, (intake) ?  SPD_INTAKE : 0);
 
 
 }
