559c24ca30e35510ad598b6ef174259d14777abd
codexcelsior <quang13012005@gmail.com>
2023-05-18 12:47:12 +0700

    Motor Control fixes

--- Training01/MotorControl.ino
+++ Training01/MotorControl.ino
@@ -11,12 +11,6 @@
 #define PWM_DC2B            5
 #define PWM_DC3A            6
 #define PWM_DC3B            7
-#define PWM_SERVO0          8
-#define PWM_SERVO1          9
-#define PWM_SERVO2          10
-#define PWM_SERVO3          11
-#define PWM_SERVO4          12
-
 
 // Động cơ DC
 #define MOT_LEFT            1
@@ -24,6 +18,9 @@
 
 // set tốc độ động cơ
 #define SPD_FAST            2047
+
+// Vận tốc đạt ở 2047 giây tức 50% chu kỳ. Tính toán thời gian để đạt tại 1 chu kỳ n% 
+//bằng cách lấy (4095 nhân n%) -1 
 
 //Khai báo linh kiện 
 Adafruit_PWMServoDriver pwm;  //Hoặc Adafruit_PWMServoDriver pwm = new Adafruit_PWMServoDriver();
@@ -42,17 +39,17 @@ void setup() { //Hàm set up chạy khởi tạo một lần khi khởi động 
 }
 
 // Hàm điều khiển động cơ DC (nhận số động cơ từ 0->3 ứng với cặp kênh PWM 0-1/2-3/4-5/6-7, giá trị tốc độ từ -4095 đến 4095)
-void ctrl_dc(uint8_t motor, int16_t speed) {
+void ctrl_dc(uint8_t motor, int16_t  ) { 
   switch(motor) {
     case 0:
-      pwm.setPWM(PWM_DC0A, 0, (speed > 0) ? speed : 0);
-      pwm.setPWM(PWM_DC0B, 0, (speed < 0) ? (-speed) : 0);
+      pwm.setPWM(PWM_DC0A, 0, (speed > 0) ? speed : 0); //Kênh PWM_DC0A, vị trí bắt đầu = 0, xung của 1 chiều là 50% đạt được sau thời gian = 2047 micro second
+      pwm.setPWM(PWM_DC0B, 0, (speed < 0) ? (-speed) : 0); 
       break;
-    case 1:
+    case 1: //Cho bánh bên tay trái
       pwm.setPWM(PWM_DC1A, 0, (speed > 0) ? speed : 0);
       pwm.setPWM(PWM_DC1B, 0, (speed < 0) ? (-speed) : 0);
       break;
-    case 2:
+    case 2: //Cho bánh bên tay phải
       pwm.setPWM(PWM_DC2A, 0, (speed > 0) ? speed : 0);
       pwm.setPWM(PWM_DC2B, 0, (speed < 0) ? (-speed) : 0);
       break;
@@ -66,6 +63,8 @@ void ctrl_dc(uint8_t motor, int16_t speed) {
 void loop() {
   //LẬP TRÌNH ĐỘNG CƠ BẢN (CÁC HÀM CƠ BẢN)
 
+  //Phần này là phần giải thích rời rạc không cần phải code phải code hẳn ra 
+
   //Khi code động cơ thì 1 chân có xung và 1 chân ko có xung 
   pwm.setPWM(0, 0, 2047); //Kênh, vị trí bắt đầu, xung của 1 chiều (0 -> 4095) 50% tốc độ là 4095 / 2 - 1
   pwm.setPWM(1, 0, 0); //Ko có xung
@@ -75,14 +74,6 @@ void loop() {
   //Set speed và chiều quay cho động cơ
   v = map(v, 0, 255, 4095, -4095); //Vì giá trị của cần Analog không tương xứng với giá trị của xung dùng để điều hướng motor
   //Ta dùng hàm map để gán hai khoảng giới hạn để input động cơ sẽ hoạt động đồng thời và trùng khợp khi ta kéo cần analog
-  if (v > 0)
-  {
-    pwm.setPWM(0, 0, v); //Giả dụ một motor có 2 kênh tương đương với 2 chân (2 dây với 2 cổng là 0 và 1) thì đặt xung vào 1 dây dây còn lại ko có xung
-    pwm.setPWM(1, 0, 0);
-  } else {
-    pwm.setPWM(0, 0, 0); //Tương tự code động cơ còn lại (4 bánh cũng tương tự như vậy)
-    pwm.setPWM(1, 0, -v);
-  }
 
 
 
@@ -90,10 +81,10 @@ void loop() {
 
   ctrl_dc(MOT_LEFT, 0); //gọi hàm ctrl_dc ra -> gán kênh của động cơ Motor trái, gán tốc độ bằng 0 (Tức là chưa chạy)
 
-  //Cách ghép hàm đẻ sử dụng DC Motor với điều khiển:
-  
+  //Cách ghép hàm đẻ sử dụng DC Motor với điều khiển PS2:
   ctrl_dc(MOT_LEFT ,map(ps2.Analog(PSS_LY), 0, 255, -SPD_FAST, SPD_FAST)); // Lấy giá trị Y từ cần Analog bên tay trái
   ctrl_dc(MOT_RIGHT,map(ps2.Analog(PSS_RY), 0, 255, SPD_FAST, -SPD_FAST)); // Lấy giá trị Y từ cần Analog bên tay trái
+
 
 }
 
